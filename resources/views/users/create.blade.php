51cfc02f4de87334321296e4bfda054c3e49f068
RomainBellaclas <romain.bellaclas@neuf.fr>
2018-11-27 22:28:50 +0100

    correctif

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -40,11 +40,5 @@
 </div>
 </div>
 </div>
-<br>
-<br>
-<br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
 @endsection
